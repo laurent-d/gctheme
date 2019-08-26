ca4db622ac712f2d269c978ab70a6c9f4e16fbb8
Laurent <lawz@free.fr>
2019-08-26 11:39:46 +0200

    charts-script ok

--- assets/js/grand-conf.js
+++ assets/js/grand-conf.js
@@ -56,6 +56,10 @@ class getCounter {
 
 lazyLoadScript("https://cdn.jsdelivr.net/npm/in-view@0.6.1/dist/in-view.min.js", "[data-section-type='charts-column']", function () {
     console.log("in-view loaded for charts colulmns");
+    inView.offset(200);
+    inView('.charts-columns').on('enter', function (chartSection) {
+      chartsreveal(chartSection);
+    });
  });
 
 
