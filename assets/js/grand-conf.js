6e5778e51031d602cc16069ec447e94f13efb5db
Laurent <lawz@free.fr>
2019-08-28 17:52:26 +0200

    lazyload in progress

--- assets/js/grand-conf.js
+++ assets/js/grand-conf.js
@@ -398,4 +398,39 @@ jQuery(document).ready(function () {
 });
 /* MAP */
 
+/* COUNTDOWN */
+  lazyLoadScript("https://laurent-d.github.io/gctheme/assets/js/jquery.countdown.js", ".countdown", function () {
+    console.log("lazyload map");
+
+  var dataDate = $('.countdown').attr('data-date');
+  $('.countdown').countdown(dataDate, function(event) {
+    var weeks = event.offset.weeks;
+    var days = event.offset.totalDays;
+    if (event.offset.weeks > 0) {
+      var clock = $(this).html(event.strftime(''
+        + '<div class="clock_bg"><div class="clock_counter">%w</div><div class="clock_label">{{ t.time_weeks }}</div></div>'
+        + '<div class="clock_bg"><div class="clock_counter">%d</div><div class="clock_label">{{ t.time_days }}</div></div>'
+        + '<div class="clock_bg"><div class="clock_counter">%H</div><div class="clock_label">{{ t.time_hours }}</div></div>'
+        + '<div class="clock_bg"><div class="clock_counter">%M</div><div class="clock_label">{{ t.time_minutes }}</div></div>'
+        + '<div class="clock_bg"><div class="clock_counter">%S</div><div class="clock_label">{{ t.time_seconds }}</div></div>')
+      );
+    } else if (event.offset.totalDays > 0) {
+      var clock = $(this).html(event.strftime(''
+        + '<div class="clock_bg"><div class="clock_counter">%d</div><div class="clock_label">{{ t.time_days }}</div></div>'
+        + '<div class="clock_bg"><div class="clock_counter">%H</div><div class="clock_label">{{ t.time_hours }}</div></div>'
+        + '<div class="clock_bg"><div class="clock_counter">%M</div><div class="clock_label">{{ t.time_minutes }}</div></div>'
+        + '<div class="clock_bg"><div class="clock_counter">%S</div><div class="clock_label">{{ t.time_seconds }}</div></div>')
+      );
+    } else {
+      var clock = $(this).html(event.strftime(''
+        + '<div class="clock_bg"><div class="clock_counter">%H</div><div class="clock_label">{{ t.time_hours }}</div></div>'
+        + '<div class="clock_bg"><div class="clock_counter">%M</div><div class="clock_label">{{ t.time_minutes }}</div></div>'
+        + '<div class="clock_bg"><div class="clock_counter">%S</div><div class="clock_label">{{ t.time_seconds }}</div></div>')
+      );
+    }
   });
+});
+
+/* COUNTDOWN */
+
+});
