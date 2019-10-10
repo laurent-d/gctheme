484883884db100f1152147f7c326f82c119a56c6
Laurent <lawz@free.fr>
2019-10-10 11:32:05 +0200

    once on charts reveal

--- assets/js/grand-conference.main.js
+++ assets/js/grand-conference.main.js
@@ -53,9 +53,11 @@ window.addEventListener("DOMContentLoaded", function (event) {
 
 
   lazyLoadScript("https://cdn.jsdelivr.net/npm/in-view@0.6.1/dist/in-view.min.js", "[data-section-type='charts-column']", function () {
-    inView.offset(200);
-    inView('.toReveal').once('enter', function (chartSection) {
-        chartsreveal(chartSection);
+    $('.toReveal').each(function () {
+      inView.offset(200);
+      inView($(this)).once('enter', function (chartSection) {
+      chartsreveal(chartSection);
+      });
     });
   });
 
