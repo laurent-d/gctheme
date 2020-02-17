c796d270d712b654dc3db5311f47a2bf5fd62f37
Laurent <lawz@free.fr>
2020-02-17 14:43:41 +0100

    ...

--- assets/js/grand-conference.main.js
+++ assets/js/grand-conference.main.js
@@ -1,5 +1,91 @@
 /* GRAND CONFERENCE ADD-ON F(X) */
 
+/**
+ * innerHTML property for SVGElement
+ * Copyright(c) 2010, Jeff Schiller
+ *
+ * Licensed under the Apache License, Version 2
+ *
+ * Minor modifications by Chris Price to only polyfill when required.
+ */
+(function(SVGElement) {
+  if (!SVGElement || 'innerHTML' in SVGElement.prototype) {
+    return;
+  }
+  var serializeXML = function(node, output) {
+    var nodeType = node.nodeType;
+    if (nodeType == 3) { // TEXT nodes.
+      // Replace special XML characters with their entities.
+      output.push(node.textContent.replace(/&/, '&amp;').replace(/</, '&lt;').replace('>', '&gt;'));
+    } else if (nodeType == 1) { // ELEMENT nodes.
+      // Serialize Element nodes.
+      output.push('<', node.tagName);
+      if (node.hasAttributes()) {
+        var attrMap = node.attributes;
+        for (var i = 0, len = attrMap.length; i < len; ++i) {
+          var attrNode = attrMap.item(i);
+          output.push(' ', attrNode.name, '=\'', attrNode.value, '\'');
+        }
+      }
+      if (node.hasChildNodes()) {
+        output.push('>');
+        var childNodes = node.childNodes;
+        for (var i = 0, len = childNodes.length; i < len; ++i) {
+          serializeXML(childNodes.item(i), output);
+        }
+        output.push('</', node.tagName, '>');
+      } else {
+        output.push('/>');
+      }
+    } else if (nodeType == 8) {
+      // TODO(codedread): Replace special characters with XML entities?
+      output.push('<!--', node.nodeValue, '-->');
+    } else {
+      // TODO: Handle CDATA nodes.
+      // TODO: Handle ENTITY nodes.
+      // TODO: Handle DOCUMENT nodes.
+      throw 'Error serializing XML. Unhandled node of type: ' + nodeType;
+    }
+  }
+  // The innerHTML DOM property for SVGElement.
+  Object.defineProperty(SVGElement.prototype, 'innerHTML', {
+    get: function() {
+      var output = [];
+      var childNode = this.firstChild;
+      while (childNode) {
+        serializeXML(childNode, output);
+        childNode = childNode.nextSibling;
+      }
+      return output.join('');
+    },
+    set: function(markupText) {
+      // Wipe out the current contents of the element.
+      while (this.firstChild) {
+        this.removeChild(this.firstChild);
+      }
+
+      try {
+        // Parse the markup into valid nodes.
+        var dXML = new DOMParser();
+        dXML.async = false;
+        // Wrap the markup into a SVG node to ensure parsing works.
+        sXML = '<svg xmlns=\'http://www.w3.org/2000/svg\'>' + markupText + '</svg>';
+        var svgDocElement = dXML.parseFromString(sXML, 'text/xml').documentElement;
+
+        // Now take each node, import it and append to this element.
+        var childNode = svgDocElement.firstChild;
+        while(childNode) {
+          this.appendChild(this.ownerDocument.importNode(childNode, true));
+          childNode = childNode.nextSibling;
+        }
+      } catch(e) {
+        throw new Error('Error parsing XML string');
+      };
+    }
+  });
+})((1, eval)('this').SVGElement);
+
+
 window.addEventListener("DOMContentLoaded", function (event) {
 
   /* Charts-columns */
@@ -10,7 +96,7 @@ window.addEventListener("DOMContentLoaded", function (event) {
       this.startCount = startCount;
       this.endCount = endCount;
       this.timer = timer * 1000 / endCount;
-      this.html = html;
+      this.textContent = textContent;
     }
 
     var _proto = getCounter.prototype;
@@ -21,7 +107,7 @@ window.addEventListener("DOMContentLoaded", function (event) {
       var self = this;
       this.interval = setInterval(function () {
         startTm += increment;
-        self.html.innerHTML = startTm + unit;
+        self.html.textContent = startTm + unit;
         if (startTm == endTm) {
           clearInterval(self.interval);
         }
@@ -34,9 +120,10 @@ window.addEventListener("DOMContentLoaded", function (event) {
     var NUMBER_TYPE = chartSection.dataset.chartsType;
     var charts = chartSection.querySelectorAll('.score');
     for (var i = 0; i < charts.length; i++) {
-      var val = charts[i].querySelector('data-chart').innerHTML;
+      var val = charts[i].querySelector('data-chart').textContent;
       console.log(val);
-      var textDisplay = charts[i].querySelector('.js-text');
+      var textDisplay = charts[i].querySelector('.js-text').textContent;
+      console.log(chart);
       if (NUMBER_TYPE == "graph") {
         console.log("graph");
         var chart = charts[i].querySelector('.js-circle');
@@ -57,7 +144,7 @@ window.addEventListener("DOMContentLoaded", function (event) {
         var unit = "";
         var inc = 1;
       } else {
-        console.log("graph");
+        console.log("nograph");
         var inc = val.length > 3 ? 10 : 1;
         var unit = "";
       }
